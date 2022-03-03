380d848d1d0a5652bd7b2ff97a17839a4939f5e2
Marcus Wiseman <md.wiseman@hotmail.com>
2022-03-03 18:18:29 +0100

    PhotoGallery component

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,42 +1,24 @@
 <template>
     <div>
         <l-tabs></l-tabs>
-
-        <div class="w-full grid gap-4 lg:grid-cols-2 2xl:grid-cols-3">
-            <img
-                v-for="(pic, i) in pics"
-                :key="i"
-                :src="require(`@/assets/images/test/${activeTab}/${pic}`)"
-                alt=""
-                class="w-full h-64 rounded-md object-cover"
-            />
-        </div>
+        <component :blok="story.content" :is="story.content.component"></component>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapGetters } from 'vuex';
 
 export default {
     name: 'gallery',
     data() {
         return {
             story: { content: {} },
-            picObj: {
-                a: ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-                b: ['1.jpg', '2.jpg', '3.jpg'],
-                jr: ['1.jpg', '2.jpg', '3.jpg'],
-            },
         };
     },
     computed: {
-        ...mapState(['pageActiveTabs']),
-        activeTab() {
-            const arr = ['a', 'b', 'jr'];
-            return arr[this.pageActiveTabs.gallery];
-        },
-        pics() {
-            return this.picObj[this.activeTab];
+        ...mapGetters(['selectedTabTeam']),
+        selectedTeamAlbums() {
+            return [];
         },
     },
     mounted() {
