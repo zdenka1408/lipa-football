0502f7d435b0500883ce61b9b3d9b788cfc0b5f5
Marcus Wiseman <md.wiseman@hotmail.com>
2022-03-12 10:45:21 +0100

    Set port 3030, add pm2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
+    server: {
+        port: 3030,
+    },
+
     head: {
         title: 'lipa-football',
         meta: [
